086cf40ee4d656fc0e6202c526121bb14c19f6b0
HugoGabrielidis16 <hugogabrielidis@gmail.com>
2025-07-14 23:48:12 +0200

    add sudo in DL script

--- script/docker_dl.sh
+++ script/docker_dl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker pull yuuuugo/mario-rl
-docker run -it --gpus all -v $(pwd):/workspace mario-rl
+sudo docker pull yuuuugo/mario-rl
+sudo docker run -it --gpus all -v $(pwd):/workspace mario-rl
